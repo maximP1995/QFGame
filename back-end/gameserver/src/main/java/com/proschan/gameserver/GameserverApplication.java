b0c814486262efde7a47ac743ab62a6a5d9f3dc4
proschan <710437653@qq.com>
2018-03-12 20:51:43 +0800

    规范了api的设计和查看问题
    规范了api的设计和查看问题

--- back-end/gameserver/src/main/java/com/proschan/gameserver/GameserverApplication.java
+++ back-end/gameserver/src/main/java/com/proschan/gameserver/GameserverApplication.java
@@ -1,14 +1,12 @@
 package com.proschan.gameserver;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@MapperScan("com.proschan.gameserver.mapper")
+//@MapperScan("com.proschan.gameserver.mapper")
 public class GameserverApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
